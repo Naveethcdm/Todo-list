5db8f3ca78f6cba7118ccb9e786917b3ce5667c9
Naveeth <cdmnaveeth@gmail.com>
2026-02-08 12:42:06 +0530

    Refactor add function to clear input field
    Reset input field after adding a new todo item.

--- todo.js
+++ todo.js
@@ -1,16 +1,18 @@
 // selecting input and ul Tag
 
-var input=document.getElementById("input")
-var ul=document.getElementById("ul")
+var input=document.getElementById("input");
+var ul=document.getElementById("ul");
 
 function add(){
     // creating new Element
-    var li=document.createElement("li")
+    var li=document.createElement("li");
 
-    li.innerHTML=input.value+"<button onclick='del(event)'>Delete</button>"
-    ul.append(li)
+    li.innerHTML=input.value+"<button onclick='del(event)'>Delete</button>";
+    ul.append(li);
+    input.value="";
 }
 
 function del(event){
     event.target.parentElement.remove()
+
 }
